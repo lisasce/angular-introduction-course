ed137ea37a331965cdd01fb02fb97dc287c3db97
lisa <lisa.66@hotmail.com>
2025-06-23 18:23:36 +0200

    Chapter Task Listing: add task-list component to app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TaskList} from './task-list/task-list';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    TaskList
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
