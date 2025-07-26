efec514c12d797309794ec0256ec46dfb219bded
lisa <lisa.66@hotmail.com>
2025-07-26 14:34:44 +0200

    Chapter create task: create route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TaskList } from "./task-list/task-list";
+import { TaskForm } from "./task-form/task-form";
+
+
+export const routes: Routes = [
+  { path: "", component: TaskList },
+  { path: "add-task", component: TaskForm },
+];
+
