21445b104878121a3f9abd18cebc1ce422e3ae90
lisa <lisa.66@hotmail.com>
2025-07-26 15:22:06 +0200

    Chapter create task: remove unused imports and variable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import {TaskList} from './task-list/task-list';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    TaskList,
     RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angular-introduction-course';
 }
