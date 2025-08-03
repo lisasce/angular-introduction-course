4ee7b6e2323cf576c3c0c8e19e9f581151f586b3
lisa <lisa.66@hotmail.com>
2025-08-03 14:27:25 +0200

    Chapter update task: add routerlink to task-list

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,11 +1,13 @@
 import {Component, inject} from '@angular/core';
 import {DatePipe} from "@angular/common";
 import {TaskService} from '../task-service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
   imports: [
-    DatePipe
+    DatePipe,
+    RouterLink
   ],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css'
