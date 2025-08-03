2f85a6020a804ead3c431f3bf864be9d40c7704f
lisa <lisa.66@hotmail.com>
2025-08-03 17:44:20 +0200

    Chapter reusable component: create alert banner

---
+++ src/app/shared/components/alert-banner/alert-banner.ts
@@ -0,0 +1,13 @@
+import {Component, input, output} from '@angular/core';
+
+@Component({
+  selector: 'app-alert-banner',
+  imports: [],
+  templateUrl: './alert-banner.html',
+  styleUrl: './alert-banner.css'
+})
+export class AlertBanner {
+  message = input.required<string>();
+  type = input.required<"info" | "error">();
+  onSubmit = output<void>();
+}
