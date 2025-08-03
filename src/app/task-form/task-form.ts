5f03d953c7c11d8f6bc0f6df0f5573829d56c30a
lisa <lisa.66@hotmail.com>
2025-08-03 16:35:21 +0200

    Chapter update task: add condition to submit, validators to form, update service

--- src/app/task-form/task-form.ts
+++ src/app/task-form/task-form.ts
@@ -1,7 +1,7 @@
-import {Component, inject} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {TaskService} from '../task-service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -12,17 +12,44 @@ import {Router} from '@angular/router';
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
-export class TaskForm {
+export class TaskForm implements OnInit {
    private taskService = inject(TaskService);
-  private router = inject(Router);
+   private router = inject(Router);
+   private route = inject(ActivatedRoute);
+  private id: string | null = null;
 
   form = new FormGroup({
-    title: new FormControl(''),
-    description: new FormControl('')
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.minLength(1)])
   });
 
+
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get("id");
+
+    if (this.id ) {
+      this.form.patchValue(this.taskService.getTask(this.id ));
+    }
+  }
+
   submit() {
-    this.taskService.addTask(this.form.value);
+    if (this.form.invalid) {
+      console.log('Your form is invalid. Please check the fields.');
+      console.log('Your Form: ', this.form.value);
+      console.log('Title Errors: ', this.form.controls.title.errors);
+      console.log('Description Errors: ', this.form.controls.description.errors);
+    }
+    if (this.id ) {
+      const existingTask = this.taskService.getTask(this.id );
+      this.taskService.updateTask({
+        ...existingTask,
+        ...this.form.value,
+      });
+    } else {
+      this.taskService.addTask(this.form.value);
+    }
     this.router.navigate(["/"]);
   }
+
 }
