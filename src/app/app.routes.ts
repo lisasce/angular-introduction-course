b27b7cd837b687a01c97eb8967d88451b2b78939
lisa <lisa.66@hotmail.com>
2025-08-03 14:27:05 +0200

    Chapter update task: add new route to update task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,6 @@ import { TaskForm } from "./task-form/task-form";
 export const routes: Routes = [
   { path: "", component: TaskList },
   { path: "add-task", component: TaskForm },
+  { path: "update/:id", component: TaskForm },
 ];
 
